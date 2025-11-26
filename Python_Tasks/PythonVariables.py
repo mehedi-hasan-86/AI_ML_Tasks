961112c4120c341c110afb41cca0649e2ce103ad
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-26 10:31:24 +0600

    PythonVariables.py

---
+++ Python_Tasks/PythonVariables.py
@@ -0,0 +1,58 @@
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+m = 4 # x is of type int
+m = "Mehedi" # x is now of type string
+print(m)
+
+a = str(3) # a will be '3'
+b = int(3) # b will be 3
+c = float(3) # c will be 3.0
+
+d = 5
+e = "Mehedi"
+print(type(d))
+print(type(e))
+
+
+x , y , z = "Orange", "Banna", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banna", "cherry"]
+x = y = z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y,z) #Python is awesome
+print(x+y+z) #Pythonisawesome
+
+x = 5
+y = "John"
+print(x,y)
+
+x = "aswsome"
+def myfunc():
+    x = "Fantastic"
+    print("Python is " + x)
+
+    myfunc()
+    print("Python is " + x)
+
+
+
+
