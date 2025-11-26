76716c1c4ed718aac9697197059fb0fc972fcbe8
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-26 16:25:37 +0600

    PythonOperators.py

---
+++ Python_Tasks/PythonOperators.py
@@ -0,0 +1,20 @@
+print(10+5)
+
+sum1 = 100+50 #150
+sum2 = sum1+250 #400
+sum3 = sum2+ sum2 #800
+print(sum3)
+
+print(10//3)
+print(10/3)
+
+print(10**3)
+
+
+x = 5
+print(1<x<10)
+print(1<x and x<10)
+
+print((6+3)-(6+3))
+
+print(100+5*3)
