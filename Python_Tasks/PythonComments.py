aa553746854728c5f2f6f6f0916741dba6a436fd
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-26 10:01:55 +0600

    PythonComments.py

---
+++ Python_Tasks/PythonComments.py
@@ -0,0 +1,17 @@
+from queue import PriorityQueue
+
+#Single line comment
+print("Hello, World")
+
+#Multiline Comment
+#This is a comment
+#Written in
+#more than just one line
+
+print("Hello , world")
+
+"""Multiline comment anoher way
+   this way is much  comfortable """
+
+
+
