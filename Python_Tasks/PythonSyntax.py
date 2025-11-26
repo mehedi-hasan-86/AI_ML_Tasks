35dd990cc94c30a8c4c69138f6e93707786b4aa9
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-26 09:52:35 +0600

    PythonSyntax.py

---
+++ Python_Tasks/PythonSyntax.py
@@ -0,0 +1,19 @@
+from traceback import print_tb
+
+print("Hello, World")
+
+if(5>2):
+    print("Five is greater than two!")
+
+
+    if 5>2:
+        print("OK")
+
+        x = 5
+        y = "Hello, World"
+        print(x)
+        print(y)
+
+        #This is a comment
+
+        print("Hey I am Mehedi"); print("Hi");
