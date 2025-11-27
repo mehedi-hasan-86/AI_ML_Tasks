2c3a7f8e69ce74ba225e9fd0a31be674b4b9e204
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-27 11:51:08 +0600

    PythonIfStatement.py

---
+++ Python_Tasks/PythonIfStatement.py
@@ -0,0 +1,94 @@
+a = 33
+b = 200
+
+if b>a:
+    print("b is greater than a")
+
+number = 15
+if number >0:
+    print("The number is positive")
+
+
+age = 20
+if age >=18:
+      print("You are an adult")
+      print("You can vote")
+      print("You have full legal rights")
+
+
+is_logged_in  = True
+if is_logged_in:
+    print("Welcome back!")
+
+
+a = 33
+b = 33
+if b>a :
+    print("b is grater than a")
+elif a==b:
+    print("a and b are equal")
+
+
+score = 75
+if score >=90:
+    print("Grade : A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade : C")
+elif score >=60:
+    print("Grade : D")
+
+age = 75
+
+if age <15:
+    print("You are a child")
+elif age<20:
+    print("You are a teenger")
+elif age<65:
+    print("You are an adult")
+elif age >=65:
+   print("You are a senior")
+
+a = 200
+b = 33
+
+if b> a:
+    print("b is greater than a")
+else:
+   print("b is not greater than a")
+
+number = 7
+if number %2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+
+temperature = 22
+
+if temperature>33:
+    print("It's hot outise!")
+elif temperature > 20:
+    print("It's ward outside")
+elif temperature > 10:
+    print("IT's cool outside")
+else:
+    print("It's cold outiside")
+
+username = "Emil"
+
+if len(username)>0:
+    print(f"Welcome, {username}!")
+else:
+    print("Error : Username cannot be empty")
+
+a = 5
+b = 2
+if a>b : print("a is grater than b")
+
+a = 200
+b = 33
+c = 500
+if a>b and c > a:
+    print("Both conditions are True")
