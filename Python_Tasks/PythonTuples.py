079eab16604647807ace02d5306a426ec4108a07
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-27 10:50:24 +0600

    PythonTuples.py

---
+++ Python_Tasks/PythonTuples.py
@@ -0,0 +1,19 @@
+
+
+thistuple = ["apple", "banna","cherry","apple"]
+print(thistuple)
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))
+
+tuple1 = ("apple", "banna", "cherry")
+tuple2 = (1,2,3,4,5)
+tuple3 = (True, False, False)
+
+tuple4 = ("abc", 34, True, 40, "male")
+print(tuple4)
+
+print(tuple4[1])
