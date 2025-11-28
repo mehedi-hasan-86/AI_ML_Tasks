4588ec999a08108fb09c26da046d1afb2d4de5c9
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-28 10:46:29 +0600

    PythonUserInput.py

---
+++ Python_Tasks/PythonUserInput.py
@@ -0,0 +1,20 @@
+# print("Enter your name: ")
+# name = input()
+# print(f"Hello {name}")
+#
+# name = input("Enter your name: ")
+# print(f"Hello {name} ")
+import math
+
+name = input("Enter you name")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal: ")
+fav2 = input("What is your favorite color : ")
+fav3 = input("What is your favorite number : ")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs")
+
+
+x = input("Enter a number :")
+y = math.sqrt(float(x))
+print(f"The square root of {x} is {y}")
+
