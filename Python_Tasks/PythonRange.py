c73ffb768114871d9b66d377b577c73f57c3bf85
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-28 09:48:52 +0600

    PythonRange.py

---
+++ Python_Tasks/PythonRange.py
@@ -0,0 +1,32 @@
+# x = range(10)
+#
+# print(x)
+#
+# print(list(x))
+#
+# x = range(3,10)
+# print(list(x))
+#
+#
+#
+# x = range(3,10,2)
+# print(x)
+#
+# print(list(x))
+#
+# for x in range(10):
+#     print(x)
+#
+# print(list(range(5)))
+# print(list(range(1,6)))
+# print(range(5,20),3)
+#
+# r = range(10)
+# print(r[2])
+# print(r[:3])
+# print(list(r[:3]))
+
+r = range(0,10,2)
+print(6 in r)
+print(7 in r)
+print(len(r))
