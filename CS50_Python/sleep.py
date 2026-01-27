81a8fbf88723574557a001084b017e7d0b03adea
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-27 18:38:58 +0600

    sleep.py

---
+++ CS50_Python/sleep.py
@@ -0,0 +1,15 @@
+def main():
+
+    n = int(input("What's n? "))
+    for i in  sheep(n):
+        print(i)
+    
+def sheep(n):
+    flock = []
+    for i in range(n):
+        flock.append("#"*i)
+
+    return flock
+
+if __name__ == "__main__":
+    main()
